cd83713b411a4bf622dfafe0af257e73b1355002
Tobias Lang <tlang@fintecsystems.com>
2019-04-08 11:17:27 +0200

    Update favicon.

--- resources/views/vendor/adminlte/master.blade.php
+++ resources/views/vendor/adminlte/master.blade.php
@@ -37,6 +37,8 @@
 
     <!-- Google Font -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 <body class="hold-transition @yield('body_class')">
