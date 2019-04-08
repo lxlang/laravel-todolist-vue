379163d341703cad4977da2590a83cc1c9097c40
Tobias Lang <tlang@fintecsystems.com>
2019-04-08 11:20:41 +0200

    Make todolist slighty wider in small viewports.

--- resources/js/components/TodoList.vue
+++ resources/js/components/TodoList.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
         <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-push-2 col-lg-6 col-lg-push-3">
+            <div class="col-xs-12 col-sm-8 col-sm-push-2 col-lg-6 col-lg-push-3 no-padding">
                 <div class="box">
                     <div class="box-header">
                         <h4>{{ this.$props.name }}</h4>
