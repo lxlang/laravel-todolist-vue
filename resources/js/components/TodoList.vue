4cd2bfbd02cf2b528f43f825819b3364033a9815
Tobias Lang <tlang@fintecsystems.com>
2019-04-09 10:15:06 +0200

    Do not display list name.

--- resources/js/components/TodoList.vue
+++ resources/js/components/TodoList.vue
@@ -3,9 +3,6 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-push-2 col-lg-6 col-lg-push-3 no-padding">
                 <div class="box">
-                    <div class="box-header">
-                        <h4>{{ this.$props.name }}</h4>
-                    </div>
                     <div class="box-body">
                         <Todo
                                 v-for="todo in todos"
